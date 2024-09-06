f46f8499b0472c1236b353d4eab8281814e7c3ca
Smaran Lal Shrestha <smaranlal134@gmail.com>
2024-09-07 00:39:37 +0530

    Time: 45 ms (48.11%), Space: 31.5 MB (35.33%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,3 +1,49 @@
+// class Solution {
+// private:
+//     bool solve(string s, string p, int i, int j, vector<vector<int>>& dp) {
+//         if (i < 0 && j < 0) {
+//             return true;
+//         }
+
+//         if (j < 0) {
+//             return false;
+//         }
+//         if (i < 0) {
+//             for (int k = 0; k <= j; k++) {
+//                 if (p[k] != '*') {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         // match
+
+//         if (p[j] == '?' || p[j] == s[i]) {
+//             return dp[i][j] = solve(s, p, i - 1, j - 1, dp);
+//         }
+//         if (p[j] == '*') {
+//             return dp[i][j] = (solve(s, p, i - 1, j, dp) ||
+//                                (solve(s, p, i, j - 1, dp)));
+//         }
+
+//         return dp[i][j] = false;
+//     }
+
+// public:
+//     bool isMatch(string s, string p) {
+//         int l1 = s.size();
+//         int l2 = p.size();
+
+//         vector<vector<int>> dp(l1, vector<int>(l2, -1));
+
+//         return solve(s, p, l1 - 1, l2 - 1, dp);
+//     }
+// };
+
 class Solution {
 private:
     bool solve(string &s, string &p, int i, int j, vector<vector<int>> &dp) {
