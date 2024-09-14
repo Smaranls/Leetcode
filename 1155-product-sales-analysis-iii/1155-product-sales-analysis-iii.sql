6f40739b34c06da556f0cbdf67a72582945bd8d2
Smaran Lal Shrestha <smaranlal134@gmail.com>
2024-09-14 22:57:03 +0530

    Time: 1225 ms (24.83%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -2,5 +2,5 @@
 select s.product_id, s.year as first_year, s.quantity as quantity, s.price 
 from sales s 
 where(product_id,year) in 
-(select product_id, min(year) as year from sales 
+(select product_id, min(year)from sales 
 group by product_id);
