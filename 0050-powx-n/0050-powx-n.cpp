0af462645f214b8e4ab4ebe52073923b3dc37a9d
Smaran Lal Shrestha <smaranlal134@gmail.com>
2024-09-23 23:40:09 +0530

    Time: 0 ms (100%), Space: 8.3 MB (43.68%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // Handle the case when n is 0
-        if (n == 0) return 1.0;
+        if (x == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 1;
+        }
 
-        // Use long long to handle the edge case of INT_MIN
-        long long exp = n; 
+        long long exp = n;
+
         if (n < 0) {
-            x = 1.0 / x; // Take reciprocal of x
-            exp = -exp; // Make exp positive
+            x = 1 / x;
+            exp = abs(exp);
         }
 
-        double result = 1.0;
-        double current_product = x;
+        double res = 1.0;
+        double product = x;
 
-        while (exp > 0) {
-            // If exp is odd, multiply the result by the current product
+        while (exp > 0) { // odd
             if (exp % 2 == 1) {
-                result *= current_product;
+                res *= product;
             }
-            // Square the current product
-            current_product *= current_product;
-            // Divide exp by 2
-            exp /= 2;
+
+            product *= product;
+
+            exp=exp/2;
         }
 
-        return result;
+        return res;
     }
 };
